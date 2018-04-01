1f76cf185a452aea02ca04a1b69e68aa037f9901
Lê Hoài Bảo <37976495+rocker216@users.noreply.github.com>
2018-04-01 20:26:29 +0700

    Add project files.

---
+++ TongHopBC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TongHopBC.Startup))]
+namespace TongHopBC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
